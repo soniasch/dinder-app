74305878cd796de390e0e0bf7b56e3c224332546
Hassan Albaaj <hassan.albaaj@tietoevry.com>
2023-04-04 13:15:13 +0200

    Added scss to the project

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,16 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_mq.scss";
+          @import "./src/styles/style.scss";
+        `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

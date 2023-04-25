600cfaf063c6df39446f963d4caf63a7cf1a9373
Hassan Albaaj <hassan.albaaj@tietoevry.com>
2023-04-25 11:52:52 +0200

    Started working on ChatGPT AI

--- backend/src/api/dog.js
+++ backend/src/api/dog.js
@@ -1,10 +1,35 @@
 const dogNames = require('dog-names')
+const { Configuration, OpenAIApi } = require("openai");
+const config = require('../../config')
 
 module.exports = {
-  index: (req, res) => {
-    res.status(200).json(
-      {name: dogNames.allRandom()}
-      )
+  index: async function (req, res) {
+    console.log('config.OPENAI_API_KEY', config.OPENAI_API_KEY);
+
+    const configuration = new Configuration({
+      apiKey: config.OPENAI_API_KEY
+    });
+    
+    const openai = new OpenAIApi(configuration);
+
+    try {
+      const response = await openai.createCompletion({
+        model: "text-ada-001",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+      });
+
+      res.status(200).json({
+        name: dogNames.allRandom(), description: response
+      })
+    } catch (e) {
+      console.log('e ', e.response.data);
+      
+      res.status(200).json({
+        name: dogNames.allRandom()
+      })
+    }
   }
 }
   
